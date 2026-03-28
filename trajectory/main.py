c56c412d8eded2c16e31844266b60da3269e6c13
Alex <sashakasyanova54@gmail.com>
2026-03-28 23:27:14 +0800

    Made distance func

--- trajectory/main.py
+++ trajectory/main.py
@@ -1,7 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from skimage.measure import label
 from pathlib import Path
 import re
+
+def distance(p1, p2):
+    return ((p2[1] - p1[1]) **2 + (p2[0] - p1[0]) **2)** 0.5
+
+def centroid(labeled, lab=1):
+    ys, xs = np.where(labeled == lab)
+    cy = np.mean(ys)
+    cx = np.mean(xs)
+    return cy,cx
 
 folder = Path('./out')
 files = []
@@ -15,17 +25,30 @@ files.sort(key=lambda x: int(re.findall(r'\d+', x.name)[0]))
 # re.findall(r'\d+', x.name)[0]) - поиск по маске всех чисел, взятие первого числа(в дан сл не надо, но на всякий)
 # sort выполняется по результату функции после key=, которая в свою очередь достает первое число из названия файла
 
-fig = plt.figure()
+centers = []
+labeled_i1 = label(np.load(files[0]))
+for i in range(1, np.max(labeled_i1) + 1):
+    centers.append(centroid(labeled_i1, i))
+
+print(distance(centers[0], centers[1] ))
 
 for file in files:
-    img = np.load(file)
+    pass
 
-    plt.clf() #очисточка
 
-    plt.imshow(img)
-    plt.axis('off')
-    plt.title(f'Показ: {file}')
 
-    plt.pause(0.05)
-
+#посомотреть последовательный вывод изображений:
+# for file in files:
+#     img = np.load(file)
+#
+#     plt.clf() #очисточка
+#
+#     plt.imshow(img)
+#     plt.axis('off')
+#     plt.title(f'Показ: {file}')
+#
+#     plt.pause(0.03)
+#
+# plt.show()
+plt.imshow(labeled_i1)
 plt.show()
