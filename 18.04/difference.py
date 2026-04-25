833a9975861f40d6e92be4ea993e77eefcb7f42d
Aleks <sashakasyanova54@gmail.com>
2026-04-25 09:56:32 +0800

    Showed difference

--- 18.04/difference.py
+++ 18.04/difference.py
@@ -11,6 +11,15 @@ diff = cv2.absdiff(cat1, cat2)
 ret, mask = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)
 mask = cv2.dilate(mask, None, iterations=2)
 
+countours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+print(len(countours))
+
+for contours in countours:
+    x,y,w,h = cv2.boundingRect(contours)
+    cv2.rectangle(cat, (x, y), (x+w, y + h), (0, 255, 0), 2)
+
+cv2.putText(cat, f'Diffs = {len(countours)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0)) 
+
 cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
 cv2.namedWindow('Difference', cv2.WINDOW_NORMAL)
 
