015c711183b231f9d1bc79913e92c4885007f8b8
Aleks <sashakasyanova54@gmail.com>
2026-05-02 10:35:15 +0800

    ...

--- 25.04/tracing.py
+++ 25.04/tracing.py
@@ -18,6 +18,7 @@ cv2.setMouseCallback('Image', on_click)
 capture = cv2.VideoCapture(0)
 lower = None
 upper = None
+positions =[]
 while True:
     ret, frame = capture.read()
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
@@ -28,16 +29,25 @@ while True:
     if clicked:
         clicked = False
         color = hsv[position[1],position[0]]
-        lower = np.clip(color*0.8, 0,255).astype("u1")
-        upper = np.clip(color*1.2, 0,255).astype("u1")
+        lower = np.clip(color*0.9, 0,255).astype("u1")
+        upper = np.clip(color*1.1, 0,255).astype("u1")
         upper[1] = 255
         upper[2] = 255
     if lower is not None:
         inr = cv2.inRange(hsv, lower, upper)
         mask = cv2.morphologyEx(inr, cv2.MORPH_CLOSE, np.ones((5, 5), dtype="uint8"))
-        counturs, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(frame, counturs, -1, (0, 255, 0), 4)
-        # cv2.circle(frame, )
         cv2.imshow('Mask', mask)
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contours = max(contours, key=cv2.contourArea)
+            (x, y), radius = cv2.minEnclosingCircle(contours)
+            if radius > 20:
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 0), 2)
+                cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 0), -1)
+                positions.append((int(x), int(y)))
+                if len(positions) == 20:
+                    positions.pop(0)
+                for i, position in enumerate(positions[:-1]):
+                    cv2.circle(frame, position, i * 2, (0,0, 100 + 155 /len(positions)*i), -1)
     cv2.imshow('Image', frame)
 cv2.destroyAllWindows()
